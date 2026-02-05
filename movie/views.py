25df6a8fc3d8fec00dc10daa393659131d711a80
ViVi09030 <vivianarango07@gmail.com>
2026-02-05 11:51:03 -0500

    cambios realizados

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Viviana Arango'})
+    #return render(request, 'home.html', {'name': 'Viviana Arango'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     #return HttpResponse('<h1>This is an About Page</h1>')
